7e09e6a7ab875ac83b81c47fdebe299c4858bceb
Kishin520 <nicokishins@hotmail.com>
2023-11-06 19:52:35 -0300

    Finalizando detalles estéticos de la vista

--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -2,7 +2,7 @@
     <ion-page>
         <ion-content class="ion-padding" fullscreen>
             <ion-grid>
-                <ion-row class="pt-24">
+                <ion-row class="pt-16">
                     <ion-col class ="flex justify-center items-center" size="2"><img  class = "w-1/2" src="/images/icons/chevron-down-outline.png" style="filter: invert(1)" /></ion-col>
                     <ion-col size="8"><h4 class="text-white text-center">Reproduciendo Canción</h4> </ion-col>
                     <ion-col class ="flex justify-center items-center" size="2"><img class ="w-1/2" src="/images/icons/icon-2.png" style="filter:invert(1)" /></ion-col>
@@ -10,12 +10,26 @@
 
                 <ion-row class="pt-16">
                     <ion-col class="flex justify-center items-center"><img src="/images/albumCovers/DiariesOfAHero.png" alt="Song Image" /></ion-col>
-
                 </ion-row>
 
+                <ion-row class="pt-6">
+                    <ion-col><h1 class="text-white text-left font-bold">Título de la canción</h1></ion-col>
+                </ion-row>
                 <ion-row>
-                    <ion-col size="10">Columna 1</ion-col>
-                    <ion-col size="2">Columna 2</ion-col>
+                    <ion-col size="10"><h1 class="text-white text-left text-sm">Autor</h1></ion-col>
+                    <ion-col size="2"><img class ="w-2/3" src="/images/icons/icon3.png" style="filter:invert(1)" /></ion-col>
+                </ion-row>
+                <ion-row>
+                    <ion-range min="0" max="100"></ion-range>
+                </ion-row>
+                <ion-row>
+                    <ion-col size="2" class="flex justify-center items-center"><img  class = "w-1/2" src="/images/icons/shuffle.png" style="filter: invert(1)" /></ion-col>
+                    <ion-col size="2" class="flex justify-center items-center"><img  class = "w-1/2 scale-x-[-1]" src="/images/icons/music-player.png" style="filter: invert(1)" /></ion-col>
+                    <ion-col size="4" class="flex justify-center items-center">
+                        <ion-button slot="icon-only" @click="playSong" :icon="isPlaying ? pauseIcon : playIcon"></ion-button>
+                    </ion-col>
+                    <ion-col size="2" class="flex justify-center items-center"><img  class = "w-1/2" src="/images/icons/music-player.png" style="filter: invert(1)" /></ion-col>
+                    <ion-col size="2" class="flex justify-center items-center"><img  class = "w-1/2" src="/images/icons/replay.png" style="filter: invert(1)" /></ion-col>
                 </ion-row>
             </ion-grid>
         </ion-content>
@@ -23,8 +37,8 @@
 </template>
 
 <script lang="ts">
-import { IonButton, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar, IonImg } from '@ionic/vue';
-import { play, pause, chevronDownOutline } from 'ionicons/icons';
+import { IonButton, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar, IonImg, IonRange } from '@ionic/vue';
+import {  playCircleOutline, pauseCircleOutline, chevronDownOutline } from 'ionicons/icons';
 
 
 export default {
@@ -37,12 +51,13 @@ export default {
         IonPage,
         IonTitle,
         IonToolbar,
+        IonRange
     },
     data() {
         return {
             isPlaying: false,
-            playIcon: play,
-            pauseIcon: pause,
+            playIcon: playCircleOutline,
+            pauseIcon:  pauseCircleOutline,
         };
     },
     methods: {
@@ -58,13 +73,19 @@ ion-content {
     --background: #000;
 }
 
-.song-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
+
+ion-range {
+    --bar-background: #ffffff;
+    --bar-background-active: #ffffff;
+    --bar-height: 4px;
+    --bar-border-radius: 6px;
+    --knob-background: #ffffff;
+    --knob-size: 20px;
+    --pin-background: #ffffff;
+    --pin-color: #ffffff;
+
 }
+
 
 .song-image {
     width: 200px;
