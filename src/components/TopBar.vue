5f60a1e58df4b37d0dd2e1b49b2b7004d9bfd58c
Kishin520 <nicokishins@hotmail.com>
2023-11-13 01:51:49 -0300

    Se evita que el usuario entre a vistas no debidas

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="flex h-[70px] bg-[#212121] sticky top-0 items-center">
-        <button class="h-[35px] w-[35px] ml-5">
-            <img src="/images/icons/audioflow-logo.png" class=" h-[35px] w-[35px]">
-        </button>
+        <router-link to="/login">
+            <button class="h-[35px] w-[35px] ml-5">
+                <img src="/images/icons/audioflow-logo.png" class=" h-[35px] w-[35px]">
+            </button>
+        </router-link>
         
-        <button class="h-[35px] w-[35px] ml-auto mr-5">
-            <img src="/images/icons/guest-pic.png" class=" h-[35px] w-[35px]">
-        </button>    
+        <router-link to="/profile">
+            <button class="h-[35px] w-[35px] ml-auto mr-5">
+                <img src="/images/icons/guest-pic.png" class=" h-[35px] w-[35px]">
+            </button>    
+        </router-link>
     </div>
 </template>
