7e6547c19b9e6d32e55ab318ceddf1decc6d0914
Kishin520 <nicokishins@hotmail.com>
2023-11-20 19:18:44 -0300

    creada la conexión del backend con la playlist

---
+++ src/api/playlist.ts
@@ -0,0 +1,20 @@
+import api from '@/services/api';
+import { useMainStore } from '@/stores/main';
+
+export async function fetchPlaylists(searchQuery: string) {
+    const response = await api.get(`/playlist/?search=${searchQuery}`);
+    return response.data;
+};
+
+export async function fetchUserPlaylists() {
+    const mainStore = useMainStore();
+    mainStore.verifyTokenValidity();
+    
+    const response = await api.get(`/playlist/${mainStore.$state.user?.id}`, {
+        headers: {
+        'Authorization': `Bearer ${mainStore.$state.token}`,
+        }
+    });
+
+    return response.data;
+};
